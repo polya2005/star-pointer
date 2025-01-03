9baca0c6647c7d612765dd58ea56a96783e1b284
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-01-03 00:12:19 -0500

    formatting change

--- main/Pointer.h
+++ main/Pointer.h
@@ -8,48 +8,51 @@
 #define CLOCKWISE 1
 #define COUNTERCLOCKWISE -1
 
-#define toSteps(rad) ((rad)*radToStep)
+#define toSteps(rad) ((rad) * radToStep)
 
-#include "Arduino.h"
 #include "AccelStepper.h"
+#include "Arduino.h"
 
-class Pointer{
-  private:
-    AccelStepper *_az{},*_al{};
-    int8_t toE{},toZ{};
-    const float radToStep{648.67939432709760002386963356506f};
-  public:
-    /// Initializes the Pointer object.
-    void begin(int az_1,int az_3,int az_2,int az_4,int8_t to_east,int al_1,int al_3,int al_2,int al_4,int8_t to_zenith);
-  
-    /// Set north and horizon reference point.
-    void setZero();
 
-    /// Point to the specified altitude
-    void setAltitude(float rad);
+class Pointer {
+ private:
+  AccelStepper *_az{}, *_al{};
+  int8_t toE{}, toZ{};
+  const float radToStep{648.67939432709760002386963356506f};
 
-    /// Point to the specified azimuth
-    void setAzimuth(float rad);
+ public:
+  /// Initializes the Pointer object.
+  void begin(int az_1, int az_3, int az_2, int az_4, int8_t to_east, int al_1,
+             int al_3, int al_2, int al_4, int8_t to_zenith);
 
-    /// Move 50 steps up
-    void up();
+  /// Set north and horizon reference point.
+  void setZero();
 
-    /// Move 50 steps down
-    void down();
+  /// Point to the specified altitude
+  void setAltitude(float rad);
 
-    /// Move 50 steps to east
-    void east();
+  /// Point to the specified azimuth
+  void setAzimuth(float rad);
 
-    /// Move 50 steps to west
-    void west();
+  /// Move 50 steps up
+  void up();
 
-    /// Run stepper motors
-    bool run();
+  /// Move 50 steps down
+  void down();
 
-    /// Stop movements
-    void stop();
-    
-    ~Pointer();
+  /// Move 50 steps to east
+  void east();
+
+  /// Move 50 steps to west
+  void west();
+
+  /// Run stepper motors
+  bool run();
+
+  /// Stop movements
+  void stop();
+
+  ~Pointer();
 };
 
 #endif
